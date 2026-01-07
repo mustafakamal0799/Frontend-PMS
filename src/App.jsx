2c12b737a32a4e6d575ade4a9cbf0057c013756f
mustafakamal0799 <mustafakama.130799@gmail.com>
2026-01-07 17:06:33 +0800

    React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Project from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/project/:id" element={<ProjectDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/project/:id" element={<ProjectDetail />} />
+    </Routes>
   );
 }
 
